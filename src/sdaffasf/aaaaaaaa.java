1191a2e802389b6c9b64b298104d2b175e69493a
王晓清 <805380422@qq.com>
2017-04-19 13:12:54 +0800

    提交相关代码

--- src/sdaffasf/aaaaaaaa.java
+++ src/sdaffasf/aaaaaaaa.java
@@ -6,3 +6,70 @@ public class aaaaaaaa {
 	//  ¡¢¡¢¡¢dfsdfsdf
   }
 }
+
+
+///***
+// * Compatible with GET and POST
+// * 
+// * @param request
+// * @return : <code>byte[]</code>
+// * @throws IOException
+// */
+//public static byte[] getRequestQuery(HttpServletRequest request)
+//		throws IOException {
+//	String submitMehtod = request.getMethod();
+//	String queryString = null;
+//
+//	if (submitMehtod.equals("GET")) {// GET
+//		queryString = request.getQueryString();
+//		String charEncoding = request.getCharacterEncoding();// charset
+//		if (charEncoding == null) {
+//			charEncoding = "UTF-8";
+//		}
+//		return queryString.getBytes(charEncoding);
+//	} else {// POST
+//		return getRequestPostBytes(request);
+//	}
+//}
+//
+///***
+// * Get request query string, form method : post
+// * 
+// * @param request
+// * @return byte[]
+// * @throws IOException
+// */
+//public static byte[] getRequestPostBytes(HttpServletRequest request)
+//		throws IOException {
+//	int contentLength = request.getContentLength();
+//	if(contentLength<0){
+//		return null;
+//	}
+//	byte buffer[] = new byte[contentLength];
+//	for (int i = 0; i < contentLength;) {
+//
+//		int readlen = request.getInputStream().read(buffer, i,
+//				contentLength - i);
+//		if (readlen == -1) {
+//			break;
+//		}
+//		i += readlen;
+//	}
+//	return buffer;
+//}
+///***
+// * Get request query string, form method : post
+// * 
+// * @param request
+// * @return
+// * @throws IOException
+// */
+//public static String getRequestPostStr(HttpServletRequest request)
+//		throws IOException {
+//	byte buffer[] = getRequestPostBytes(request);
+//	String charEncoding = request.getCharacterEncoding();
+//	if (charEncoding == null) {
+//		charEncoding = "UTF-8";
+//	}
+//	return new String(buffer, charEncoding);
+//}
